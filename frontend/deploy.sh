d94e74f78eb2f7755baf39fbe0335237bd36509a
022-024 <022-024@yandex.ru>
2023-10-31 10:24:29 +0000

    gl4 frontend deploy att 6

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -r /opt/sausage-store/static/sausage-store/frontend || true
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz  ${NEXUS_REPO_URL}/${NEXUS_REPO_URL_FRONTEND}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/sausage-store/ || true
+sudo tar -zxf sausage-store.tar.gz --directory /opt/sausage-store/static/ || true
 #Это я на всякий случай (понятия не имею какие права выдает разархивирование)
 sudo chmod 755 /opt/sausage-store/static/sausage-store/frontend || true
 sudo systemctl daemon-reload

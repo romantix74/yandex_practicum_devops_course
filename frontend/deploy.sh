00fe05544911f9b81349fc60a646e005fd68a5d0
022-024 <022-024@yandex.ru>
2024-01-11 15:15:15 +0000

    glava_7  practice 1 att 9, frontend att 16

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,8 +4,8 @@ sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-frontend || true
 sudo docker run -d --name sausage-frontend \
-     # -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \
      -p 8080:80 \
      --network=sausage_network \
      --restart=always \
      "${CI_REGISTRY_IMAGE}"/sausage-frontend:$CI_COMMIT_SHA
+     # -v /tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf \

4b9ad4989f9dc8d63810954163a271c075136987
022-024 <022-024@yandex.ru>
2024-01-11 13:44:24 +0000

    glava_7  practice 1 att 9, frontend att 11

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
-sudo docker run --rm -d --name sausage-backend \
+sudo docker run  -d --name sausage-backend \
      --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
      --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,7 +2,7 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
-sudo docker -f sausage-frontend || true
+sudo docker rm -f sausage-frontend || true
 sudo docker run -d --name sausage-frontend \
      --network=sausage_network \
      --restart=always \ 

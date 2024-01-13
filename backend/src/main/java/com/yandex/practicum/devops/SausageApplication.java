ea9926cb335e4e372b6e3eaf32c59be8ded9c1ec
022-024 <022-024@yandex.ru>
2024-01-13 16:53:45 +0000

    glava 7.2 att 1 in master

--- backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
+++ backend/src/main/java/com/yandex/practicum/devops/SausageApplication.java
@@ -19,13 +19,13 @@ public class SausageApplication {
     @Bean
     CommandLineRunner runner(ProductService productService) {
         return args -> {
-            productService.save(new Product(1L, "Сливочная", 320.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg"));
-            productService.save(new Product(2L, "Особая", 179.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg"));
-            productService.save(new Product(3L, "Молочная", 225.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg"));
-            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg"));
-            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg"));
-            productService.save(new Product(6L, "Еврейская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
-            productService.save(new Product(6L, "Русская", 189.00, "https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg"));
+            productService.save(new Product(1L, "Сливочная", 320.00, "https://storage.yandexcloud.net/bucket-std-022-024/6.jpg"));
+            productService.save(new Product(2L, "Особая", 179.00, "https://storage.yandexcloud.net/bucket-std-022-024/5.jpg"));
+            productService.save(new Product(3L, "Молочная", 225.00, "https://storage.yandexcloud.net/bucket-std-022-024/4.jpg"));
+            productService.save(new Product(4L, "Нюренбергская", 315.00, "https://storage.yandexcloud.net/bucket-std-022-024/3.jpg"));
+            productService.save(new Product(5L, "Мюнхенская", 330.00, "https://storage.yandexcloud.net/bucket-std-022-024/2.jpg"));
+            productService.save(new Product(6L, "Еврейская", 189.00, "https://storage.yandexcloud.net/bucket-std-022-024/1.jpg"));
+            productService.save(new Product(6L, "Русская", 189.00, "https://storage.yandexcloud.net/bucket-std-022-024/1.jpg"));
         };
     }
 }

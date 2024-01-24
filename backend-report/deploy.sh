bcff97d9979efc6edd01714b299b1e830d00a4b5
022-024 <022-024@yandex.ru>
2024-01-24 10:39:24 +0000

    glava8.1.2 att 7 deploy by compose

---
+++ backend-report/deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -xe
+sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage_network || true
+sudo docker rm -f sausage-backend || true
+sudo VERSION=$(VERSION) docker compose up
+
+# sudo docker run  -d --name sausage-backend \
+#      --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
+#      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
+#      --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
+#      --env SPRING_DATA_MONGODB_URI="${SPRING_DATA_MONGODB_URI}" \
+#      --network=sausage_network \
+#      --restart=always \
+#      "${CI_REGISTRY_IMAGE}"/sausage-backend:$CI_COMMIT_SHA
+     
